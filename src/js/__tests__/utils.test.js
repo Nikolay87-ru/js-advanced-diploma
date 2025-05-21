c6790da5c4b4099656cefd558dfd4f6e2774b2cb
Neto test <nikolajeliseev834@gmail.com>
2025-05-21 20:29:20 +0300

    Добавлены дополнительные тесты для лучшего покрытия

--- src/js/__tests__/utils.test.js
+++ src/js/__tests__/utils.test.js
@@ -16,7 +16,27 @@ describe("calcTileType", () => {
   test("should return 'left'", () => {
     expect(calcTileType(7, 7)).toBe("left");
   });
+
+  test("should return 'top-right'", () => {
+    expect(calcTileType(7, 8)).toBe("top-right");
+  });
+  
+  test("should return 'bottom-left'", () => {
+    expect(calcTileType(56, 8)).toBe("bottom-left");
+  });
+  
+  test("should return 'right'", () => {
+    expect(calcTileType(15, 8)).toBe("right");
+  });
+  
+  test("should return 'center'", () => {
+    expect(calcTileType(36, 8)).toBe("center");
+  });
 });
 
-
+test("should return correct values for 5x5 board", () => {
+  expect(calcTileType(0, 5)).toBe("top-left");
+  expect(calcTileType(4, 5)).toBe("top-right");
+  expect(calcTileType(24, 5)).toBe("bottom-right");
+});
 
